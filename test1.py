b4e2cdb29a794d734c28ada40aac3763721a0682
SangbumChoi <danielsejong55@gmail.com>
2019-12-21 18:52:53 +0900

    example of gui especially control window

--- test1.py
+++ test1.py
@@ -1,18 +1,19 @@
-# coding: utf-8
-
 import sys
-from PyQt5 import QtWidgets
-from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget
 
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-class Form(QtWidgets.QDialog):
-    def __init__(self, parent=None):
-        QtWidgets.QDialog.__init__(self, parent)
-        self.ui = uic.loadUi("form.ui")
-        self.ui.show()
+    def initUI(self):
+        self.setWindowTitle('My First Application')
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    w = Form()
-    sys.exit(app.exec())
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
