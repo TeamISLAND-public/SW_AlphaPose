cfd5b2da56af05c74cd1cd64248d4ad0199f0c82
SangbumChoi <danielsejong55@gmail.com>
2019-12-25 17:22:10 +0900

    [File Change] New UI/untitled.ui experiements held

---
+++ UI/init.py
@@ -0,0 +1,8 @@
+def __init__(self):
+    super().__init__()
+    self.title = 'PyQt5 button - pythonspot.com'
+    self.left = 10
+    self.top = 10
+    self.width = 320
+    self.height = 200
+    self.initUI()
