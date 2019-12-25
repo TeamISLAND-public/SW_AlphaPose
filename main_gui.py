f7263c67bbf25fb3b80324c9589a44c3cd37159c
SangbumChoi <danielsejong55@gmail.com>
2019-12-25 17:18:04 +0900

    [Code Test] How to make sub python file linked with main python file button

--- main_gui.py
+++ main_gui.py
@@ -1,38 +1,47 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtCore, QtGui, QtWidgets
 from Functions import Add
+import sys
 
 
-class App(QWidget):
+class Ui_MainWindow(object):
 
-    def __init__(self):
-        super().__init__()
-        self.title = 'PyQt5 button - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 320
-        self.height = 200
-        self.initUI()
+    def setupUi(self, MainWindow):
+        MainWindow.resize(506, 312)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+        # adding pushbutton
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(200, 150, 93, 28))
 
-        button = QPushButton('PyQt5 button', self)
-        button.setToolTip('This is an example button')
-        button.move(100, 70)
-        button.clicked.connect(Add.runscript)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(140, 90, 221, 20))
 
-        self.show()
+        # keeping the text of label empty before button get clicked
+        self.label.setText("")
 
-    @pyqtSlot()
-    def on_click(self):
-        print('PyQt5 button click')
+        # adding signal and slot
+        self.pushButton.clicked.connect(self.changelabeltext)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Push Button"))
+
+    def changelabeltext(self):
+        # changing the text of label after button get clicked
+        Add.changelabeltext(self)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+
     sys.exit(app.exec_())
