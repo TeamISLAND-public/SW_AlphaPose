6665445df68018aa81786006ef2144d65ec60734
kdh0324 <kdh0324@postech.ac.kr>
2020-01-08 21:54:16 +0900

    add video button which can change the video

--- main_gui.py
+++ main_gui.py
@@ -14,6 +14,9 @@ class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.fileList = QVBoxLayout()
+        self.fileNameList = []
+        self.fileButtonList = []
         self.positionSlider = QSlider(Qt.Horizontal)
         self.playButton = QPushButton()
         self.volumeSlider = QSlider(Qt.Vertical)
@@ -23,7 +26,7 @@ class MyApp(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.resize(700, 500)
+        self.resize(1100, 500)
         self.menu()
         self.statusBar()
 
@@ -45,6 +48,7 @@ class MyApp(QMainWindow):
         volumeBox.addWidget(self.volumeText)
 
         layout = QHBoxLayout()
+        layout.addLayout(self.fileList)
         layout.addLayout(volumeBox)
         layout.addLayout(vLayout)
 
@@ -55,28 +59,72 @@ class MyApp(QMainWindow):
         self.show()
 
     def open_video(self):
+        # if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+        #     self.mediaPlayer.stop()
         filename = QFileDialog.getOpenFileName(self, "Open file", os.getcwd(), "Video files(*.mp4 *.mkv *.avi)")
-        # self.cap = cv2.VideoCapture(filename[0])
+
+        # if Video is already opened
+        if filename[0] in self.fileNameList:
+            errorbox = QMessageBox()
+            errorbox.question(self, "Error Message", "Video is already opened", QMessageBox.Ok)
+            return
+
+        # if user doesn't select the video
+        if not filename[0]:
+            return
+
         self.playButton.setEnabled(True)
+
+        # set video
         self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(filename[0])))
-        self.maxVolume = self.mediaPlayer.volume()
+        self.mediaPlayer.setVideoOutput(self.videoWidget)
+
+        # add the button which express each video
         try:
-            self.videoPlayer()
+            self.fileNameList.append(filename[0])
+            # fileButton = FileButton(filename[0])
+            fileButton = QPushButton(filename[0])
+            fileButton.clicked.connect(lambda: self.change_video(fileButton.text()))
+            self.fileButtonList.append(fileButton)
+            self.fileList.addWidget(fileButton)
+            self.fileList.setAlignment(Qt.AlignTop)
         except Exception as ex:
             print(ex)
 
-    def videoPlayer(self):
-        self.playButton.clicked.connect(self.play)
+        # set max volume of video
+        self.maxVolume = self.mediaPlayer.volume()
 
-        self.positionSlider.sliderMoved.connect(self.setPosition)
-        self.volumeSlider.sliderMoved.connect(self.setVolume)
+        self.videoPlayer()
 
+    def change_video(self, text):
+        self.playButton.setEnabled(True)
+
+        # set video
+        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(str(text))))
         self.mediaPlayer.setVideoOutput(self.videoWidget)
+
+        # set max volume of video
+        self.maxVolume = self.mediaPlayer.volume()
+
+        self.videoPlayer()
+
+    def videoButton(self):
+        raise NotImplementedError
+
+    def videoPlayer(self):
         self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
+
+        # video controller
+        self.playButton.clicked.connect(self.play)
+        self.positionSlider.sliderMoved.connect(self.setPosition)
         self.mediaPlayer.positionChanged.connect(self.controlVideo)
         self.mediaPlayer.durationChanged.connect(self.videoDuration)
+
+        # audio controller
+        self.volumeSlider.sliderMoved.connect(self.setVolume)
         self.mediaPlayer.volumeChanged.connect(self.controlVolume)
         self.volumeDuration(self.mediaPlayer.volume())
+
         self.mediaPlayer.error.connect(self.handleError)
 
     def play(self):
@@ -93,7 +141,7 @@ class MyApp(QMainWindow):
 
     def controlVolume(self, volume):
         self.volumeSlider.setValue(volume)
-        self.volumeText.setText("{:.1f}%".format((volume/self.maxVolume) * 100))
+        self.volumeText.setText("{:.1f}%".format((volume / self.maxVolume) * 100))
 
     def volumeDuration(self, duration):
         self.volumeSlider.setRange(0, duration)
@@ -114,7 +162,11 @@ class MyApp(QMainWindow):
 
     def handleError(self):
         self.playButton.setEnabled(False)
+        self.positionSlider.setRange(0, 0)
+        self.volumeSlider.setRange(0, 0)
+        self.volumeText.clear()
         self.setStatusTip("Error: " + self.mediaPlayer.errorString())
+        print(self.mediaPlayer.errorString())
 
     # def saveVideo(self):
     #     fourcc = cv2.VideoWriter_fourcc(*"DIVX")
@@ -153,7 +205,6 @@ class MyApp(QMainWindow):
 
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     ex = MyApp()
     sys.exit(app.exec_())
