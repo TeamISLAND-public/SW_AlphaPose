3de22185ee790243d73950eee2b40fcfc82471ff
kdh0324 <kdh0324@postech.ac.kr>
2020-01-13 16:47:55 +0900

    Devloping the function to stream the video with the webcam

--- main_gui.py
+++ main_gui.py
@@ -70,12 +70,11 @@ class MyApp(QMainWindow):
         # if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
         #     self.mediaPlayer.stop()
         filename = QFileDialog.getOpenFileName(self, "Open file", os.getcwd(), "Video files(*.mp4 *.mkv *.avi)")
-        self.videoTable.setColumnCount(1)
 
         # if Video is already opened
         if filename[0] in self.fileNameList:
             errorbox = QMessageBox()
-            errorbox.question(self, "Error Message", "Video is already opened", QMessageBox.Ok)
+            errorbox.warning(self, "Error Message", "Video is already opened", QMessageBox.Ok)
             return
 
         # if user doesn't select the video
@@ -83,6 +82,7 @@ class MyApp(QMainWindow):
             return
 
         self.playButton.setEnabled(True)
+        self.videoTable.setColumnCount(1)
 
         # set video
         self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(filename[0])))
@@ -197,6 +197,14 @@ class MyApp(QMainWindow):
     #     out.release()
     #     cv2.destroyAllWindows()
 
+    def record_video(self):
+        recordWindow = RecordApp.getInstance()
+        recordWindow.show()
+        recordWindow.start()
+        # self.close()
+        # recordWindow.videoCaptureThread.join()
+        # self.show()
+
     def menu(self):
         menu = self.menuBar()
         menu_file = menu.addMenu("&File")
@@ -206,6 +214,15 @@ class MyApp(QMainWindow):
         video_open.setShortcut("Ctrl+O")
         video_open.setStatusTip("Open the video file")
         video_open.triggered.connect(self.open_video)
+
+        webCam_open = QAction("Record", self)
+        webCam_open.setShortcut("Ctrl+R")
+        webCam_open.setStatusTip("Record with webcam")
+        webCam_open.triggered.connect(self.record_video)
+
+        video_new = QMenu("New", self)
+        video_new.addAction(video_open)
+        video_new.addAction(webCam_open)
 
         file_exit = QAction("Exit", self)
         file_exit.setShortcut("Ctrl+Q")
@@ -217,8 +234,83 @@ class MyApp(QMainWindow):
         file_save.setStatusTip("Save the video file")
         # file_save.triggered.connect(self.saveVideo)
 
-        menu_file.addAction(video_open)
+        menu_file.addMenu(video_new)
         menu_file.addAction(file_exit)
+
+
+class RecordApp(QMainWindow):
+    __instance = None
+
+    @classmethod
+    def __getInstance(cls):
+        return cls.__instance
+
+    @classmethod
+    def getInstance(cls):
+        cls.__instance = cls()
+        cls.getInstance = cls.__getInstance
+        return cls.__instance
+
+    def __init__(self):
+        super().__init__()
+
+        self.running = False
+        self.recordButton = QPushButton("Stop")
+        self.videoStream = QLabel()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Record Video")
+
+        self.recordButton.clicked.connect(self.stop)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.videoStream)
+        layout.addWidget(self.recordButton)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        self.resize(640, 500)
+
+    def start(self):
+        if self.running:
+            errorbox = QMessageBox()
+            errorbox.warning(self, "Error Message", "Webcam is already opened.", QMessageBox.Ok)
+            return
+
+        self.videoCaptureThread = threading.Thread(target=self.run)
+        self.videoCaptureThread.daemon = True
+        self.videoCaptureThread.start()
+
+    def stop(self):
+        try:
+            self.running = False
+            self.videoCaptureThread.join()
+            self.cap.release()
+            self.close()
+        except Exception as ex:
+            print(ex)
+
+    def run(self):
+        self.cap = cv2.VideoCapture(0)
+        self.running = True
+
+        while self.running:
+            ret, frame = self.cap.read()
+            if ret:
+                img = frame
+                qImg = QImage(img.data, img.shape[1], img.shape[0], QImage.Format_BGR888)
+                pixMap = QPixmap.fromImage(qImg)
+                self.videoStream.setPixmap(pixMap)
+            else:
+                errorbox = QMessageBox()
+                errorbox.warning(self, "Error Message", "Cannot read frame.", QMessageBox.Ok)
+                self.running = False
+                self.cap.release()
+                self.close()
+                break
 
 
 if __name__ == '__main__':
