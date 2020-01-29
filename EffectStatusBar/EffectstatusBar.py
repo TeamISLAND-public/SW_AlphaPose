e662122775de0c4763a1da9357411ebffffe777f
SangbumChoi <danielsejong55@gmail.com>
2020-01-29 17:07:48 +0900

    almost done for interaction bewteen statusbar and effectbar

--- EffectStatusBar/EffectstatusBar.py
+++ EffectStatusBar/EffectstatusBar.py
@@ -1,13 +1,12 @@
-from PyQt5.QtWidgets import QPushButton, QProgressBar, QScrollArea, QSlider, QListWidgetItem, QRadioButton, QAction
-from PyQt5.QtWidgets import QTableWidget, QFormLayout, QAction, QGroupBox, QDialog, QVBoxLayout
+from PyQt5.QtWidgets import QTableWidget,QTableWidgetItem, QAbstractItemView, QAction, QApplication, QHeaderView
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import pyqtSlot, pyqtSignal
 
 from EffectStatusBar.RangeSlider import QRangeSlider
+import sys
 
-class EffectStatusBar(QDialog):
+class EffectStatusBar(QTableWidget):
     effect_queue_list = []
-    delete_button_list = []
     returnValue = pyqtSignal(int)
     val = 0
     def __init__(self):
@@ -15,61 +14,64 @@ class EffectStatusBar(QDialog):
         self.init_ui()
 
     def init_ui(self):
-        self.formLayout =QFormLayout()
-        groupBox = QGroupBox("EffectStatusBar")
-        groupBox.setLayout(self.formLayout)
-        scroll = QScrollArea()
-        scroll.setWidget(groupBox)
-        scroll.setWidgetResizable(True)
-        # scroll.setFixedHeight(200)
-        layout = QVBoxLayout(self)
-        layout.addWidget(scroll)
-        self.show()
+        super().__init__()
 
-    def delete_button_finding(self, delete_list):
-        for delete_button in delete_list:
-            delete_button.clicked.connect(self.test)
+        self.effect_queue_list = []
+        self.setRowCount(0)
+        self.setColumnCount(1)
+        self.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.cell_right_clicked()
+        self.show()
 
     def cell_right_clicked(self):
         self.setContextMenuPolicy(Qt.ActionsContextMenu)
-        delete_action = QAction("Delete", self)
+        delete_action = QAction("Delete Effect", self)
         self.addAction(delete_action)
-        delete_action.triggered.connect(self.button0_clicked_change_value)
+        delete_action.triggered.connect(self.delete_effect_queue_list)
 
-    def test(self):
-        print('a')
-
-    def button0_clicked_change_value(self):
-        if self.delete_button_list == []:
-            self.returnValue.emit(self.val)
-        else:
-            self.formlayout.removeRow(self.val)
-            del self.delete_button[self.val]
-            del self.effect_queue[self.val]
-            print(self.delete_button)
-            self.returnValue.emit(self.val)
-            if self.val == 0:
-                self.val = self.val
-            else:
-                self.val = self.val - 1
-        print(self.val)
+    def delete_effect_queue_list(self):
+        # if self.effect_queue_list == []:
+        #     self.returnValue.emit(self.val)
+        # else:
+        #     print(self.effect_queue_list)
+        #     self.returnValue.emit(self.val)
+        #     if self.val == 0:
+        #         self.val = self.val
+        #     else:
+        #         self.val = self.val - 1
+        #     self.formLayout.itemAt(self.val).widget().setParent(None)
+        #     del self.effect_queue_list[self.val]
+        #     print(self.effect_queue_list)
+        for i in self.selectedItems():
+            print(i)
+            # self.effect_queue_list.remove(self.item(i.row(), 0).text())
+            self.removeRow(i.row())
 
 # This line is for EffectBar to EffectStatusBar connection
     def make_connection(self, class_object):
         class_object.sentValue.connect(self.EffectBar_Inter_EffectStatusBar)
 
     @pyqtSlot(int, int)
-    def EffectBar_Inter_EffectStatusBar(self, val, type, effect_queue_list = effect_queue_list,
-                                        delete_button_list = delete_button_list):
+    def EffectBar_Inter_EffectStatusBar(self, val, type, effect_queue_list = effect_queue_list):
         # self.val = val
         # self.type = type
-        # currentRowCount = self.rowCount()
-        effect_queue_list.append(QRangeSlider())
-        # delete_button_list.append(QPushButton("D"))
-        # self.insertRow(currentRowCount)
-        # self.setItem(0, currentRowCount, QTableWidgetItem)
-        # self.effect_queue_list[val].setValue(val)
-        # print(self.formlayout.itemAt(val))
-        # self.layou
-        self.formLayout.addWidget(effect_queue_list[val])
-        A = 1
+        # effect_queue_list.append(QRangeSlider())
+        # self.formLayout.addWidget(effect_queue_list[val])
+        # self.formLayout.update()
+        # print(val, type)
+        #
+        effect_queue_list.append(val)
+        print(effect_queue_list)
+        currentRowCount = self.rowCount()
+        print(currentRowCount)
+        self.insertRow(currentRowCount)
+        self.setItem(0, currentRowCount, QTableWidgetItem("{}".format(currentRowCount)))
+        self.setCellWidget(currentRowCount, 0, QRangeSlider())
+        self.horizontalHeader().setStretchLastSection(True)
+
+        # self.resizeColumnsToContents()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = EffectStatusBar()
+    sys.exit(app.exec_())
