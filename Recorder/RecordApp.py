3a1b17bc5cc2c995be8d26db93a6ab0a0e58306c
kdh0324 <kdh0324@postech.ac.kr>
2020-02-03 19:32:48 +0900

    solve error #7 which is error about recording video

--- Recorder/RecordApp.py
+++ Recorder/RecordApp.py
@@ -1,6 +1,7 @@
 import sys
 import cv2
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QLabel, QWidget, QVBoxLayout, QMessageBox, QApplication
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QLabel, QWidget, QVBoxLayout, QMessageBox, QApplication, QFileDialog
+from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QImage, QPixmap
 import threading
 
@@ -26,6 +27,7 @@ class RecordApp(QMainWindow):
         self.recordButton = QPushButton("Record")
         self.stopButton = QPushButton("Stop")
         self.videoStream = QLabel()
+        self.timer = QTimer()
         self.initUI()
 
     def initUI(self):
@@ -53,46 +55,68 @@ class RecordApp(QMainWindow):
             errorbox.warning(self, "Error Message", "Webcam is already opened.", QMessageBox.Ok)
             return
 
-        self.videoCaptureThread = threading.Thread(target=self.run)
-        self.videoCaptureThread.daemon = True
-        self.videoCaptureThread.start()
+        self.cap = cv2.VideoCapture(0)
+        self.run()
+
+        # self.videoCaptureThread = threading.Thread(target=self.run)
+        # self.videoCaptureThread.daemon = True
+        # self.videoCaptureThread.start()
 
     def stop(self):
-        self.running = False
-        self.videoCaptureThread.join()
+        # self.running = False
+        # self.videoCaptureThread.join()
         self.cap.release()
+        self.timer.stop()
         self.close()
 
     def run(self):
-        self.cap = cv2.VideoCapture(0)
-        self.running = True
+        self.timer.timeout.connect(self.stream)
+        self.timer.start(50)
+        # self.cap = cv2.VideoCapture(0)
+        # self.running = True
+        #
+        # while self.running:
+        #     ret, frame = self.cap.read()
+        #     if ret:
+        #         img = frame
+        #         qImg = QImage(img.data, img.shape[1], img.shape[0], QImage.Format_BGR888)
+        #         pixMap = QPixmap.fromImage(qImg)
+        #         self.videoStream.setPixmap(pixMap)
+        #
+        #         if self.recording:
+        #             try:
+        #                 self.video_writer.write(frame)
+        #             except Exception as ex:
+        #                 print(ex)
+        #     else:
+        #         errorbox = QMessageBox()
+        #         errorbox.warning(self, "Error Message", "Cannot read frame.", QMessageBox.Ok)
+        #         self.running = False
+        #         self.cap.release()
+        #         self.close()
+        #         break
 
-        while self.running:
-            ret, frame = self.cap.read()
-            if ret:
-                img = frame
-                qImg = QImage(img.data, img.shape[1], img.shape[0], QImage.Format_BGR888)
-                pixMap = QPixmap.fromImage(qImg)
-                self.videoStream.setPixmap(pixMap)
+    def stream(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            return
 
-                if self.recording:
-                    try:
-                        self.video_writer.write(frame)
-                    except Exception as ex:
-                        print(ex)
-            else:
-                errorbox = QMessageBox()
-                errorbox.warning(self, "Error Message", "Cannot read frame.", QMessageBox.Ok)
-                self.running = False
-                self.cap.release()
-                self.close()
-                break
+        flipImg = cv2.flip(frame, 1)
+        qImg = QImage(flipImg.data, flipImg.shape[1], flipImg.shape[0], QImage.Format_BGR888)
+        pixMap = QPixmap.fromImage(qImg)
+        self.videoStream.setPixmap(pixMap)
+
+        if self.recording:
+            self.video_writer.write(flipImg)
 
     def record(self):
+        self.timer.stop()
         self.recording = True
         self.recordButton.setEnabled(False)
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        self.video_writer = cv2.VideoWriter("output.avi", fourcc, 20, (640, 480))
+        name = QFileDialog.getSaveFileName(self, "Save File", "output.avi", "Videos(*.avi)")
+        self.video_writer = cv2.VideoWriter(name[0], fourcc, 20, (640, 480))
+        self.timer.start()
 
 
 if __name__ == "__main__":
